0b9fdd893ff584f4268929de7195637ec8cbb8ce
phild <zhe.dong@icahn.mssm.edu>
2019-01-30 18:16:39 -0500

    make median filter optional

--- minian/initialization.py
+++ minian/initialization.py
@@ -101,21 +101,22 @@ def gmm_refine(varr, seeds, q=(0.1, 99.9), n_components=2, valid_components=1, m
     return seeds, varr_pv, gmm
 
 
-def pnr_refine(varr, seeds, noise_freq=0.25, thres=1.5, q=(0.1, 99.9)):
+def pnr_refine(varr, seeds, noise_freq=0.25, thres=1.5, q=(0.1, 99.9), med_wnd=None):
     print("selecting seeds") 
     varr_sub = varr.sel(
         spatial=[tuple(hw) for hw in seeds[['height', 'width']].values])
     varr_sub = varr_sub.chunk(dict(frame=-1, spatial='auto'))
-    varr_base = xr.apply_ufunc(
-        median_filter,
-        varr_sub,
-        input_core_dims=[['frame']],
-        output_core_dims=[['frame']],
-        dask='parallelized',
-        kwargs=dict(size=501),
-        vectorize=True,
-        output_dtypes=[varr_sub.dtype])
-    varr_sub = (varr_sub - varr_base).persist()
+    if med_wnd:
+        varr_base = xr.apply_ufunc(
+            median_filter,
+            varr_sub,
+            input_core_dims=[['frame']],
+            output_core_dims=[['frame']],
+            dask='parallelized',
+            kwargs=dict(size=med_wnd),
+            vectorize=True,
+            output_dtypes=[varr_sub.dtype])
+        varr_sub = (varr_sub - varr_base).persist()
     print("computing peak-noise ratio")
     but_b, but_a = butter(2, noise_freq, btype='high', analog=False)
     varr_noise = xr.apply_ufunc(
