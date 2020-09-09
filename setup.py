409d69e182c7375fc1214f04ee1e4bc6b991b299
Zoran Sinnema <zsinnema@gmail.com>
2020-09-09 09:56:07 +0200

    #83 .

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import setuptools
 
 NAME = "MiniAn"
-VERSION = "0.0.5"
+
+with open("VERSION") as f:
+    VERSION = f.read().splitlines()[0]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -15,15 +17,18 @@ def _get_requirements(requirement_file):
 
 setuptools.setup(
     name=NAME,
-    version='v0.2.0',
+    version=VERSION,
     author="Denise Cai",
     author_email="denisecai@gmail.com",
     description="MiniAn is an analysis pipeline and visualization tool inspired by both CaImAn and MIN1PIPE package specifically for Miniscope data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DeniseCaiLab/minian",
-    install_requires=_get_requirements('requirements.txt'),
-    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=_get_requirements("requirements.txt"),
+    packages=setuptools.find_packages(),
+    project_urls={
+        "Documentation": "https://github.com/DeniseCaiLab/minian",
+    }
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
