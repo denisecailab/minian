af6faec2ee9215f21d9c2d8cbe859d34373f7acd
Zoran Sinnema <zsinnema@gmail.com>
2020-10-12 09:18:14 +0200

    chore: added download of images needed for pipeline_noted notebook

--- minian/install.py
+++ minian/install.py
@@ -1,48 +1,41 @@
-import argparse
 import os
 import requests
 
-PIPELINE_FILES = ("pipeline.ipynb","pipeline_noted.ipynb","cross-registration.ipynb")
-DEMO_FILES = [f"demo_movies/msCam{i}.avi" for i in range(1,11)]
+PIPELINE_FILES = ('pipeline.ipynb','pipeline_noted.ipynb','cross-registration.ipynb')
+IMAGE_FILES = ('img/first_tem_drop_v2.PNG', 'img/folder_structure.png', 'img/pnr_param_v2.png'
+              ,'img/sparse_panel_spatial_update.PNG', 'img/first_tem_drop_v2.PNG', 'img/first_tem_param.png')
+DEMO_FILES = [f'demo_movies/msCam{i}.avi' for i in range(1,11)]
+
+def _ask_branch() -> str:
+  return input("use branch [master]: ") or "master"
 
 def _get_file(filename: str, branch: str):
-  if os.path.isfile(f"{filename}"):
-    print(f"File {filename} already exists, skipping install of this file.")
+  if os.path.isfile(f'{filename}'):
+    print(f'File {filename} already exists, skipping install of this file.')
     return
 
-  r = requests.get(f"https://raw.github.com/DeniseCaiLab/minian/{branch}/{filename}")
+  r = requests.get(f'https://raw.github.com/DeniseCaiLab/minian/{branch}/{filename}')
   if r.status_code == 200:
-    with open(f"{filename}","wb") as f:
+    with open(f'{filename}','wb') as f:
       for chunk in r.iter_content(2048):
         f.write(chunk)
-    print(f"File {filename} installed.")
+    print(f'File {filename} installed.')
 
-def demo(branch: str):
+def demo():
   try:
-    os.mkdir("demo_movies")
-    print("Installing demo movies")
+    os.mkdir('demo_movies')
+    print('Installing demo movies')
+    branch = _ask_branch()
     for file in DEMO_FILES:
       _get_file(file, branch)
   except OSError:
-    print("Creation of the directory demo_movies failed, not installing.")
+    print('Creation of the directory demo_movies failed, not installing.')
 
-def pipeline(branch: str):
-  print("Installing pipeline notebooks")
+def pipeline():
+  print('Installing pipeline notebooks')
+  branch = _ask_branch()
   for file in PIPELINE_FILES:
     _get_file(file, branch)
-
-def main():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--notebooks", action="store_true", help="Installs the pipeline notebooks")
-        parser.add_argument("--demo", action="store_true", help="Installs the demo movies")
-        parser.add_argument("-b", action="store", default="master", help="Git repo branch name, default master")
-        args = parser.parse_args()
-
-        branch = args.b
-        print(f"Using branch: {branch}")
-
-        if args.notebooks:
-          pipeline(branch)
-
-        if args.demo:
-          demo(branch)
+  os.mkdir('img')
+  for file in IMAGE_FILES:
+    _get_file(file, branch)
