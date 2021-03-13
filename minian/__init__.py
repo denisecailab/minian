3c2d0b0118e075680dc39510a428ca1646e10c65
phildong <zhe.dong@icahn.mssm.edu>
2021-03-13 11:04:13 -0500

    add global config for dask

---
+++ minian/__init__.py
@@ -0,0 +1,28 @@
+import dask as da
+import os
+from .utilities import custom_arr_optimize, custom_delay_optimize
+
+da.config.set(
+    array_optimize=custom_arr_optimize, delayed_optimize=custom_delay_optimize
+)
+# setting fuse width ref: https://github.com/dask/dask/issues/5105
+da.config.set(
+    **{
+        "distributed.worker.memory.target": 0.25,
+        "distributed.worker.memory.spill": 0.5,
+        "distributed.worker.memory.pause": 0.9,
+        "distributed.worker.memory.terminate": 0.95,
+        "distributed.admin.log-length": 100,
+        "distributed.scheduler.transition-log-length": 100,
+        "optimization.fuse.ave-width": 3,
+        # "optimization.fuse.subgraphs": False,
+        # "distributed.scheduler.allowed-failures": 1,
+        # "array.slicing.split_large_chunks": False,
+    }
+)
+# ref: https://github.com/dask/dask/issues/3530
+# on linux, after conda installing jemalloc, one can use the following line to
+# get around threaded scheduler memory leak issue.
+# os.environ["LD_PRELOAD"] = "~/.conda/envs/minian-dev/lib/libjemalloc.so"
+# alternatively one can limit the malloc pool, which is the default for minian
+os.environ["MALLOC_MMAP_THRESHOLD_"] = "16384"
