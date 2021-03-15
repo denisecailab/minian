e3f4527655ff52c869e5c90d900b1b191bd0c4c5
phildong <zhe.dong@icahn.mssm.edu>
2021-03-14 23:56:27 -0400

    update tests

--- minian/test/test_pipeline.py
+++ minian/test/test_pipeline.py
@@ -16,11 +16,11 @@ def test_pipeline_notebook():
     assert minian_ds.sizes["frame"] == 2000
     assert minian_ds.sizes["height"] == 480
     assert minian_ds.sizes["width"] == 752
-    assert minian_ds.sizes["unit_id"] == 401
-    assert (minian_ds["shifts"].sum("frame").values == np.array([-1154, -363])).all()
-    assert int(minian_ds["max_proj"].sum().compute()) == 1663176
-    assert int(minian_ds["C"].sum().compute()) == 152918218
-    assert int(minian_ds["S"].sum().compute()) == 1366809
-    assert int(minian_ds["A"].sum().compute()) == 401
+    assert minian_ds.sizes["unit_id"] == 390
+    assert (minian_ds["shifts"].sum("frame").values == np.array([-957, 2512])).all()
+    assert int(minian_ds["max_proj"].sum().compute()) == 1645143
+    assert int(minian_ds["C"].sum().compute()) == 128420990
+    assert int(minian_ds["S"].sum().compute()) == 1424842
+    assert int(minian_ds["A"].sum().compute()) == 390
     assert os.path.exists("./demo_movies/minian_mc.mp4")
     assert os.path.exists("./demo_movies/minian.mp4")
