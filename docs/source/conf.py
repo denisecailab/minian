243fe3a7f819b5de1d2c97a3898fe1ecc0feb8e9
phildong <zhe.dong@icahn.mssm.edu>
2021-03-28 22:07:46 -0400

    add custom autodoc extension to handle numba.jit decorated functions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,18 +25,17 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
+sys.path.append(os.path.abspath("exts"))
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.todo',
-    'sphinx.ext.napoleon',
+    "numbadoc",
 ]
 
+numba_jit_prefix = ""
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
