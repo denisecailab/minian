1a7dbc589ac0fdf9c96857de9c6235bd3fed907b
phildong <zhe.dong@icahn.mssm.edu>
2021-03-28 22:11:23 -0400

    update sphinx config and change theme to pydata

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,29 +17,38 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'MiniAn'
-copyright = '2020, Denise J. Cai'
-author = 'Denise J. Cai'
+project = "MiniAn"
+copyright = "2020, Denise J. Cai"
+author = "Denise J. Cai"
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
 
+sys.path.insert(0, os.path.abspath("../../"))
 sys.path.append(os.path.abspath("exts"))
+
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.todo",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
     "sphinx_autodoc_typehints",
     "numbadoc",
 ]
 
+napoleon_use_rtype = False
 typehints_document_rtype = False
 numba_jit_prefix = ""
+
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -52,11 +61,11 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
+# html_theme = "alabaster"
+html_theme = "pydata_sphinx_theme"
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
