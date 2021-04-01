232a52c4b9198ffc5d47c242b0d4f32693b4e14e
phild <zhe.dong@icahn.mssm.edu>
2021-04-01 18:48:06 -0400

    add custom styling

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -101,3 +101,5 @@ html_theme = "pydata_sphinx_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_css_files = ["custom.css"]
