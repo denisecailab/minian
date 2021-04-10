22413583afda65536fa944d9982f31e421b6ef12
phildong <zhe.dong@icahn.mssm.edu>
2021-04-10 17:06:17 -0400

    update version specification

--- minian/__init__.py
+++ minian/__init__.py
@@ -2,6 +2,8 @@ import dask as da
 import os
 from .utilities import custom_arr_optimize, custom_delay_optimize
 
+__version__ = "0.1.1"
+
 da.config.set(
     array_optimize=custom_arr_optimize, delayed_optimize=custom_delay_optimize
 )
