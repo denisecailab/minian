57f9576a677aaf0ff516a7f90ee2fe27a83a70c4
phildong <35715936+phildong@users.noreply.github.com>
2021-04-11 16:26:52 -0400

    fix: fix install script
    Update install script to properly handle versions and directories.

--- minian/install.py
+++ minian/install.py
@@ -13,34 +13,38 @@ NOTEBOOK_FILES = [
 DEMO_FILES = [f"demo_movies/msCam{i}.avi" for i in range(1, 11)] + [
     f"demo_data/session{i}/minian.nc" for i in range(1, 3)
 ]
+VERSION = "0.1.1"
 
 
-def _get_file(filename: str, branch: str):
+def _get_file(filename: str, version: str):
     if os.path.isfile(f"{filename}"):
         print(f"File {filename} already exists, skipping install of this file.")
         return
+    for vv in [version, "v" + version]:
+        r = requests.get(f"https://raw.github.com/DeniseCaiLab/minian/{vv}/{filename}")
+        if r.status_code == 200:
+            parent_dir = os.path.dirname(filename)
+            if parent_dir:
+                os.makedirs(parent_dir, exist_ok=True)
+            with open(f"{filename}", "wb") as f:
+                for chunk in r.iter_content(2048):
+                    f.write(chunk)
+            print(f"File {filename} installed.")
+            break
+    else:
+        print(f"File {filename} not found with version {version}, skipping.")
 
-    r = requests.get(f"https://raw.github.com/DeniseCaiLab/minian/{branch}/{filename}")
-    if r.status_code == 200:
-        with open(f"{filename}", "wb") as f:
-            for chunk in r.iter_content(2048):
-                f.write(chunk)
-        print(f"File {filename} installed.")
 
-
-def demo(branch: str):
-    os.makedirs("demo_movies", exist_ok=True)
-    os.makedirs("demo_data", exist_ok=True)
+def demo(version: str):
     print("Installing demo data")
     for file in DEMO_FILES:
-        _get_file(file, branch)
+        _get_file(file, version)
 
 
-def notebook(branch: str):
-    os.makedirs("img", exist_ok=True)
+def notebook(version: str):
     print("Installing notebooks")
     for file in NOTEBOOK_FILES:
-        _get_file(file, branch)
+        _get_file(file, version)
 
 
 def main():
@@ -50,18 +54,18 @@ def main():
     )
     parser.add_argument("--demo", action="store_true", help="Installs the demo data")
     parser.add_argument(
-        "-b",
+        "-v",
         action="store",
-        default="master",
-        help="Git repo branch name, default master",
+        default=VERSION,
+        help="Git repo branch or tag name, default {}".format(VERSION),
     )
     args = parser.parse_args()
 
-    branch = args.b
-    print(f"Using branch: {branch}")
+    version = args.v
+    print(f"Using version: {version}")
 
     if args.notebooks:
-        notebook(branch)
+        notebook(version)
 
     if args.demo:
-        demo(branch)
+        demo(version)
