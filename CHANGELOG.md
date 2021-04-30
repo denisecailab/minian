57d0ccd165d18d0ff00c3f1cf8eb16cff059b2fd
phild <zhe.dong@icahn.mssm.edu>
2021-04-30 17:10:49 -0400

    bump: version 1.0.0rc0 → 1.0.0rc1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
+## v1.0.0rc1 (2021-04-30)
+
+### Feat
+
+- graph based resolving of mappings
+
+### Fix
+
+- fix pipeline when `subset` is used
+
 ## v1.0.0rc0 (2021-04-11)
 
 Candidate for first public release.
--- minian/__init__.py
+++ minian/__init__.py
@@ -2,7 +2,7 @@ import dask as da
 import os
 from .utilities import custom_arr_optimize, custom_delay_optimize
 
-__version__ = "1.0.0rc0"
+__version__ = "1.0.0rc1"
 
 da.config.set(
     array_optimize=custom_arr_optimize, delayed_optimize=custom_delay_optimize
