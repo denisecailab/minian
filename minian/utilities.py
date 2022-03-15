f6d85c45bb7543f0c2941291a34a68cba2e84cd6
Matthias Klumpp <matthias@tenstral.net>
2022-03-15 16:59:23 +0100

    Get correct frame amount for video files without nb_frames property

--- minian/utilities.py
+++ minian/utilities.py
@@ -225,7 +225,15 @@ def load_avi_lazy(fname: str) -> darr.array:
     video_info = next(s for s in probe["streams"] if s["codec_type"] == "video")
     w = int(video_info["width"])
     h = int(video_info["height"])
-    f = int(video_info["nb_frames"])
+    if "nb_frames" in video_info:
+        f = int(video_info["nb_frames"])
+    else:
+        r_fps = video_info["r_frame_rate"]
+        num, denom = r_fps.split("/")
+        fps = float(num) / float(denom)
+        f = float(probe["format"]["duration"]) * fps
+        f = np.round(f).astype(int)
+
     return da.array.from_delayed(
         da.delayed(load_avi_ffmpeg)(fname, h, w, f), dtype=np.uint8, shape=(f, h, w)
     )
